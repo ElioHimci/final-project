a6913e4519f56fb4b11ab37f260e625d2312fb49
ElioHimci <108124059+ElioHimci@users.noreply.github.com>
2022-11-29 10:13:09 +0100

    ndryshime tek importet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import {Button} from '@mui/material'
 import Container from '@mui/material/Container';
+import ThemeProvider from '@mui/material/themeCssVarsAugmentation'
+import theme from './styles/theme';
 function App() {
 
   
   return (
+    <ThemeProvider theme = {theme}>
     <Container maxWidth  = 'xl'>
 
       {/* 
@@ -19,7 +22,7 @@ function App() {
       */}
      <Button variant='contained'> hello</Button>
      </Container>
-    
+    </ThemeProvider> 
   );
 }
 
