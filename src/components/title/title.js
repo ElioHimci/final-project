875b46b6b6d3e357a7e4fe27c8ae8d0577a10c3b
ElioHimci <108124059+ElioHimci@users.noreply.github.com>
2022-12-14 13:44:52 +0100

    stilizim i title (desktop / mobile)

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,10 +1,21 @@
-import { Box } from '@mui/material';
+import { useTheme } from '@emotion/react';
+import { Box, useMediaQuery } from '@mui/material';
 import React from 'react';
 import { AppbarHeader } from '../../styles/appbar';
+import { Colors } from '../../styles/theme';
 
 const Title = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('md'))
   return (
-    <Box display={"flex"} justifyContent={"center"} sx = {{padding : 4}}>
+    <Box 
+    color = {Colors.primary}
+    width= '100%'
+    display={"flex"} 
+    justifyContent={"center"} 
+    sx = {{padding : 4 }} 
+     fontSize = {matches ?  10 : 15 }
+    alignItems='center'>
         <AppbarHeader>OUR PRODUCTS</AppbarHeader>
     </Box>
   )
